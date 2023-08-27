6aee930b2ec8d48a30569c3156674f784581aa19
Tallysing <tallysing@tutanota.com>
2023-08-27 08:51:39 +0200

    temporary standalone js file

---
+++ script/global.js
@@ -0,0 +1,6 @@
+import { toggleDetails } from "toggleDetails";
+
+var project1=document.getElementsByTagName("figure")[0];
+
+project1.addEventListener("click", toggleDetails(project1));
+
---
+++ script/toggleDetails.js
@@ -0,0 +1,7 @@
+
+export function toggleDetails(project) {
+    
+    var details=document.getElementsByTagName("details")[0];
+
+    console.log(details);
+}
